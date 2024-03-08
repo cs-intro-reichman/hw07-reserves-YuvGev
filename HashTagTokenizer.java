5553202fa7e1aec472e76f0d54f55d78279d1857
YuvGev <154268127+YuvGev@users.noreply.github.com>
2024-03-08 17:46:55 +0200

    HW7
    pushhhhh

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -13,6 +13,9 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
+		for (int i =0; i < dictionary.length; i++){
+			dictionary[i] = in.readLine();
+		}
 
 		// Your code here
 
@@ -21,6 +24,12 @@ public class HashTagTokenizer {
 
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
+		for (int i =0; i < dictionary.length; i++){
+			if (word.equals(dictionary[i])){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -31,9 +40,13 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
-
+		hashtag = hashtag.toLowerCase();
         for (int i = 1; i <= N; i++) {
-		
+			if (existInDictionary(hashtag.substring(0, i), dictionary)){
+				System.out.println(hashtag.substring(0, i));
+				breakHashTag(hashtag.substring(i), dictionary);
+				break;
+			}
         }
     }
 
--- SpellChecker.java
+++ SpellChecker.java
@@ -12,16 +12,39 @@ public class SpellChecker {
 
 	public static String tail(String str) {
 		// Your code goes here
+		return str.substring(1);
 	}
+
+	public static char head(String str) {
+		return str.charAt(0);
+	}
+
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+		if (word1.length() == 0) {
+			return word2.length();
+		}
+		if (word2.length() == 0) {
+			return word1.length();
+		}
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		if (head(word1) == head(word2)) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+		int calculation = Math.min(Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2))),
+				levenshtein(tail(word1), tail(word2)));
+		return 1 + calculation;
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
+		for (int i = 0; i < dictionary.length; i++){
+			dictionary[i] = in.readLine();
+		}
 
 		// Your code here
 
@@ -30,6 +53,21 @@ public class SpellChecker {
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		int distance;
+		int min = 1000;
+		String closeWord = "";
+		for (int i = 0; i < dictionary.length; i++){
+			distance = levenshtein(word, dictionary[i]);
+			if (distance < min){
+				min = distance;
+				if (distance <= threshold){
+					closeWord = dictionary[i];
+				} else {
+					closeWord = word;
+				}
+			}
+		}
+		return closeWord;
 	}
 
 }
